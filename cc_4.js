78982f91e217528554ed2f6c9896469e3ab9d135
Hayden Finney <haydenfinney@usf.edu>
2025-09-17 18:56:32 -0400

    created cc_4 and pre products list

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+const products = [
+    {name:"",category:"",price:"",inventory:""},
+    {name:"",category:"",price:"",inventory:""},
+    {name:"",category:"",price:"",inventory:""},
+    {name:"",category:"",price:"",inventory:""},
+    {name:"",category:"",price:"",inventory:""},
+];
+  
